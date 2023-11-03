6d46abeca1cebef2e77a4af1fb5103871e691e7f
Hamza (Asahi Ubuntu) <hamza72x@gmail.com>
2023-11-03 23:04:43 +0600

    minor changes

--- init.lua
+++ init.lua
@@ -323,7 +323,7 @@ end, { desc = '[S]earch [O]pen directory' })
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'bash', 'fish', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
